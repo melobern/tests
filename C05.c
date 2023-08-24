9f82068ff362c72de85f8ac57caef073b89321bd
Melodie Bernard <mbernard@z2r11p2.42lyon.fr>
2023-08-24 18:43:29 +0200

    better path

--- C05.c
+++ C05.c
@@ -6,20 +6,20 @@
 /*   By: mbernard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/15 18:04:28 by mbernard          #+#    #+#             */
-/*   Updated: 2023/08/24 18:31:04 by mbernard         ###   ########.fr       */
+/*   Updated: 2023/08/24 18:43:18 by mbernard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
-#include "../ex00/ft_iterative_factorial.c"
-#include "../ex01/ft_recursive_factorial.c"
-#include "../ex02/ft_iterative_power.c"
-#include "../ex03/ft_recursive_power.c"
-#include "../ex04/ft_fibonacci.c"
-#include "../ex05/ft_sqrt.c"
-//#include "../ex06/ft_is_prime.c"
-#include "../ex07/ft_find_next_prime.c"
-//#include "../ex08/ft_ten_queens_puzzle.c"
+#include "ex00/ft_iterative_factorial.c"
+#include "ex01/ft_recursive_factorial.c"
+#include "ex02/ft_iterative_power.c"
+#include "ex03/ft_recursive_power.c"
+#include "ex04/ft_fibonacci.c"
+#include "ex05/ft_sqrt.c"
+//#include "ex06/ft_is_prime.c"
+#include "ex07/ft_find_next_prime.c"
+//#include "ex08/ft_ten_queens_puzzle.c"
 int	main(void)
 {
 	printf("ITERATIVE FACTORIELLE\n"); 
